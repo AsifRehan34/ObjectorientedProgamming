a63245d109ea1b0f9777f51b927f5ef6071f6ff4
Muhammad Asif <you@example.com>
2024-08-11 11:46:39 +0500

    object type and parameter added

--- Draw_using_user_input.py
+++ Draw_using_user_input.py
@@ -1,21 +1,17 @@
-import turtle
 from turtle import *
-def draw_triangle():
+drawing=Turtle()
+def draw_triangle(drawing,size,angle):
     for i in range(3):
-        triangle.forward(100)
-        triangle.left(120)
-def draw_circle():
-    turtle.circle(radius=30)
+        drawing.forward(size)
+        drawing.left(angle)
+def draw_circle(drawing,radius):
+    drawing.circle(radius)
 
-
-
-triangle=Turtle()
-Circle = Turtle()
 userinput=input("Circle or triangle:").lower()
 if userinput=="triangle":
-    draw_triangle()
+    draw_triangle(drawing,200,90)
 elif userinput=="circle":
-    draw_circle()
+    draw_circle(drawing,50)
 else:
     print("invalid input")
 exitonclick()
